adde64b46575bd022123fb0f1c31e7ffc7befd08
Mert Özbek <75756442+mrtqzbek11@users.noreply.github.com>
2022-01-05 12:55:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+      var menu = document.querySelector('.nav-list');
+      function opennav() {
+      menu.style.top = "0";
+      }
+
+      function closenav() {
+        menu.style.top = "-1000px";
+        }
